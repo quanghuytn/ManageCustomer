c76ae9c78009e8476b98a84af635061a4bcf56a1
quanghuytn <96805674+quanghuytn@users.noreply.github.com>
2024-05-09 21:55:43 +0700

    Add manage customer winform

---
+++ ManageCustomer/ManageCustomer/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace ManageCustomer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgProduct = new System.Windows.Forms.DataGridView();
+            this.TxtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSearch2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgProduct
+            // 
+            this.dgProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProduct.Location = new System.Drawing.Point(63, 288);
+            this.dgProduct.Name = "dgProduct";
+            this.dgProduct.RowHeadersWidth = 51;
+            this.dgProduct.RowTemplate.Height = 29;
+            this.dgProduct.Size = new System.Drawing.Size(747, 216);
+            this.dgProduct.TabIndex = 0;
+            // 
+            // TxtId
+            // 
+            this.TxtId.Location = new System.Drawing.Point(150, 31);
+            this.TxtId.Name = "TxtId";
+            this.TxtId.Size = new System.Drawing.Size(165, 27);
+            this.TxtId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(464, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(537, 34);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(165, 27);
+            this.txtName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Price:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(150, 92);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(165, 27);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stock:";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(537, 88);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(165, 27);
+            this.txtStock.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Image:";
+            // 
+            // txtImage
+            // 
+            this.txtImage.Location = new System.Drawing.Point(150, 145);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(165, 27);
+            this.txtImage.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(464, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Category:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(101, 216);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(252, 216);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(402, 216);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 29);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(551, 216);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(94, 29);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(691, 216);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 29);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(563, 141);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(151, 28);
+            this.cboCategory.TabIndex = 18;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(741, 33);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnSearch.TabIndex = 19;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSearch2
+            // 
+            this.btnSearch2.Location = new System.Drawing.Point(741, 86);
+            this.btnSearch2.Name = "btnSearch2";
+            this.btnSearch2.Size = new System.Drawing.Size(94, 29);
+            this.btnSearch2.TabIndex = 20;
+            this.btnSearch2.Text = "SEARCH2";
+            this.btnSearch2.UseVisualStyleBackColor = true;
+            this.btnSearch2.Click += new System.EventHandler(this.btnSearch2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 535);
+            this.Controls.Add(this.btnSearch2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cboCategory);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.dgProduct);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgProduct;
+        private TextBox TxtId;
+        private Label label1;
+        private Label label2;
+        private TextBox txtName;
+        private Label label3;
+        private TextBox txtPrice;
+        private Label label4;
+        private TextBox txtStock;
+        private Label label5;
+        private TextBox txtImage;
+        private Label label6;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnReset;
+        private Button btnExit;
+        private ComboBox cboCategory;
+        private Button btnSearch;
+        private Button btnSearch2;
+    }
+}
---
+++ ManageCustomer/ManageCustomer/Form1.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using ManageCustomer.Models;
+
+namespace ManageCustomer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (MySaleDBContext context = new MySaleDBContext())
+            {
+                // lấy dữ liệu từ bảng product
+                //C1 - LinQ
+                // var data = context.Products.Select(p=>new
+                // {
+                //     ID = p.ProductId,
+                //     Name = p.ProductName,
+                //     Price = p.UnitPrice,
+                //     Stock = p.UnitsInStock,
+                //     Image = p.Image,
+                //     CategoryName = p.Category.CategoryName
+                // }).OrderBy(p=> p.Price)
+                //.ThenByDescending(p=>p.Stock)
+                // .ToList();
+
+                //C2 - LinQ
+                var data = (from p in context.Products
+                                //where p.ProductName.Contains(txtName.Text)
+                            orderby p.UnitPrice, p.UnitsInStock descending
+                            select new
+                            {
+                                ID = p.ProductId,
+                                Name = p.ProductName,
+                                Price = p.UnitPrice,
+                                Stock = p.UnitsInStock,
+                                Image = p.Image,
+                                CategoryName = p.Category.CategoryName
+                            }).ToList();
+
+                // add vào dgProduct
+                dgProduct.DataSource = data;
+
+                // lấy dữ liệu từ bảng category
+                var data2 = context.Categories.ToList();
+
+                cboCategory.DataSource = data2;
+                cboCategory.DisplayMember = "CategoryName";
+                cboCategory.ValueMember = "CategoryId";
+
+                //binding dữ liệu lên các text
+                TxtId.DataBindings.Clear();
+                TxtId.DataBindings.Add("Text", data, "ID");
+
+                txtName.DataBindings.Clear();
+                txtName.DataBindings.Add("Text", data, "Name");
+
+                txtPrice.DataBindings.Clear();
+                txtPrice.DataBindings.Add("Text", data, "Price");
+
+                txtStock.DataBindings.Clear();
+                txtStock.DataBindings.Add("Text", data, "Stock");
+
+                txtImage.DataBindings.Clear();
+                txtImage.DataBindings.Add("Text", data, "Image");
+
+                cboCategory.DataBindings.Clear();
+                cboCategory.DataBindings.Add("Text", data, "CategoryName");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (MySaleDBContext context = new MySaleDBContext())
+            {
+                //tao doi tuong moi
+                Product p = new Product()
+                {
+                    ProductName = txtName.Text,
+                    UnitPrice = Decimal.Parse(txtPrice.Text),
+                    UnitsInStock = Int32.Parse(txtStock.Text),
+                    Image = txtImage.Text,
+                    CategoryId = (int)cboCategory.SelectedValue
+                };
+
+                // add vao db
+                context.Products.Add(p);
+                if (context.SaveChanges() > 0)
+                {
+                    MessageBox.Show("Add success");
+                }
+                else
+                {
+                    MessageBox.Show("Add fail");
+                }
+                LoadData();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (MySaleDBContext context = new MySaleDBContext())
+            {
+                //tao doi tuong moi
+                Product p = context.Products.FirstOrDefault(p => p.ProductId == Int32.Parse(TxtId.Text));
+                if (p != null)
+                {
+                    //update
+                    p.ProductName = txtName.Text;
+                    p.UnitPrice = Decimal.Parse(txtPrice.Text);
+                    p.UnitsInStock = Int32.Parse(txtStock.Text);
+                    p.Image = txtImage.Text;
+                    p.CategoryId = (int)cboCategory.SelectedValue;
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show("Update success");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update fail");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Not found");
+                }
+                LoadData();
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySaleDBContext context = new MySaleDBContext())
+            {
+                //tao doi tuong moi
+                Product p = context.Products.FirstOrDefault(p => p.ProductId == Int32.Parse(TxtId.Text));
+                if (p != null)
+                {
+                    //update
+                    context.Products.Remove(p);
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show("Delete success");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Delete fail");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Not found");
+                }
+                LoadData();
+
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (MySaleDBContext context = new MySaleDBContext())
+            {
+                //C2 - LinQ
+                var data = (from p in context.Products
+                            where p.ProductName.Contains(txtName.Text)
+                            orderby p.UnitPrice, p.UnitsInStock descending
+                            select new
+                            {
+                                ID = p.ProductId,
+                                Name = p.ProductName,
+                                Price = p.UnitPrice,
+                                Stock = p.UnitsInStock,
+                                Image = p.Image,
+                                CategoryName = p.Category.CategoryName
+                            }).ToList();
+
+                // add vào dgProduct
+                dgProduct.DataSource = data;
+
+                // lấy dữ liệu từ bảng category
+                var data2 = context.Categories.ToList();
+
+                cboCategory.DataSource = data2;
+                cboCategory.DisplayMember = "CategoryName";
+                cboCategory.ValueMember = "CategoryId";
+
+                //binding dữ liệu lên các text
+                TxtId.DataBindings.Clear();
+                TxtId.DataBindings.Add("Text", data, "ID");
+
+                txtName.DataBindings.Clear();
+                txtName.DataBindings.Add("Text", data, "Name");
+
+                txtPrice.DataBindings.Clear();
+                txtPrice.DataBindings.Add("Text", data, "Price");
+
+                txtStock.DataBindings.Clear();
+                txtStock.DataBindings.Add("Text", data, "Stock");
+
+                txtImage.DataBindings.Clear();
+                txtImage.DataBindings.Add("Text", data, "Image");
+
+                cboCategory.DataBindings.Clear();
+                cboCategory.DataBindings.Add("Text", data, "CategoryName");
+            }
+        }
+
+        private void btnSearch2_Click(object sender, EventArgs e)
+        {
+            using (MySaleDBContext context = new MySaleDBContext())
+            {
+                //C2 - LinQ
+                var data = (from p in context.Products
+                            where p.ProductName.Contains(txtName.Text) && p.Image.Contains(txtImage.Text)
+                            orderby p.UnitPrice, p.UnitsInStock descending
+                            select new
+                            {
+                                ID = p.ProductId,
+                                Name = p.ProductName,
+                                Price = p.UnitPrice,
+                                Stock = p.UnitsInStock,
+                                Image = p.Image,
+                                CategoryName = p.Category.CategoryName
+                            }).ToList();
+
+                // add vào dgProduct
+                dgProduct.DataSource = data;
+
+                // lấy dữ liệu từ bảng category
+                var data2 = context.Categories.ToList();
+
+                cboCategory.DataSource = data2;
+                cboCategory.DisplayMember = "CategoryName";
+                cboCategory.ValueMember = "CategoryId";
+
+                //binding dữ liệu lên các text
+                TxtId.DataBindings.Clear();
+                TxtId.DataBindings.Add("Text", data, "ID");
+
+                txtName.DataBindings.Clear();
+                txtName.DataBindings.Add("Text", data, "Name");
+
+                txtPrice.DataBindings.Clear();
+                txtPrice.DataBindings.Add("Text", data, "Price");
+
+                txtStock.DataBindings.Clear();
+                txtStock.DataBindings.Add("Text", data, "Stock");
+
+                txtImage.DataBindings.Clear();
+                txtImage.DataBindings.Add("Text", data, "Image");
+
+                cboCategory.DataBindings.Clear();
+                cboCategory.DataBindings.Add("Text", data, "CategoryName");
+            }
+        }
+    }
+}
